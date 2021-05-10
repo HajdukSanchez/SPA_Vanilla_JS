4ac356655b5969e2111c6dae4360bdbc5f2de889
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-05-10 10:39:01 -0500

    :art: Class 11

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,18 @@
-const Home = () => {
+import getData from '../utils/getData';
+
+const Home = async () => {
+  const characters = await getData();
   // We create the template literal
   const view = `
     <div class="Characters">
-      <article class="Character-item">
-        <a href="#/1/">
-          <img scr="image" alt="name">
-          <h2>Name</h2>
-        </a>
-      </article>
+      ${characters.results.map(character => `
+        <article class="Character-item">
+          <a href="#/${character.id}/">
+            <img scr="${character.image}" alt="${character.name}">
+            <h2>${character.name}</h2>
+          </a>
+        </article>
+        `).join('')}
     </div>
   `;
   return view;
---
+++ src/utils/getData.js
@@ -0,0 +1,15 @@
+const API = 'https://rickandmortyapi.com/api/character';
+
+const getData = async (id) => {
+  // If there is and ID, this const is going to have another URL
+  const apiURL = id ? `${API}${id}` : API;
+  try {
+    const response = await fetch(apiURL);
+    const data = await response.json(); // Change response to a JSON data object
+    return data;
+  } catch (error) {
+    console.error('Fecth Error:', error);
+  }
+};
+
+export default getData;
